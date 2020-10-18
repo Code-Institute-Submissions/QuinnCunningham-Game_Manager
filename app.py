4aa6b32f1160c38259ddf9f4a65e6733c255c50f
QuinnCunningham <quinncunningham10@outlook.com>
2020-10-18 12:58:52 +0000

    Heroku APP

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'games'
@@ -15,9 +15,11 @@ mongo = PyMongo(app)
 def get_games():
     return render_template("games.html", games=mongo.db.games.find())
 
+
 @app.route('/addgame')
 def addgame():
     return render_template("add_game.html")
+
 
 @app.route('/enter_games', methods=['POST'])
 def enter_games():
@@ -25,22 +27,22 @@ def enter_games():
     games.insert_one(request.form.to_dict())
     return redirect(url_for('get_games'))
 
+
 @app.route('/edit_game/<games_id>')
 def edit_game(games_id):
-    the_game = mongo.db.games.find_one({"_id":ObjectId(games_id)})
+    the_game = mongo.db.games.find_one({"_id": ObjectId(games_id)})
     return render_template('editgame.html', games=the_game,)
+
 
 @app.route('/update_games/<games_id>', methods=['POST'])
 def update_games(games_id):
     games = mongo.db.games
-    games.update({'_id': ObjectId(games_id)}, 
-    {
-    'game_name': request.form.get('game_name'),
-    'game_date': request.form.get('game_date'),
-    'game_type': request.form.get('game_type'),
-    'game_rating': request.form.get('game_rating'),
-    'game_description': request.form.get('game_descirption')
-    
+    games.update({'_id': ObjectId(games_id)}, {
+        'game_name': request.form.get('game_name'),
+        'game_date': request.form.get('game_date'),
+        'game_type': request.form.get('game_type'),
+        'game_rating': request.form.get('game_rating'),
+        'game_description': request.form.get('game_descirption')
     })
     return redirect(url_for('get_games'))
 
